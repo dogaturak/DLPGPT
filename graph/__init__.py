29f9c2b9dda4616d02207c324edf65658ed5b51f
dogaturak <d.turak@student.rug.nl>
2026-06-09 01:53:43 +0200

    small bug fix

--- graph/__init__.py
+++ graph/__init__.py
@@ -2,6 +2,6 @@ from graph.node import Node
 from graph.edge import Edge
 from graph.graph import Graph
 from graph.llm_node import LLMNode
-from graph.operations import  FileAnalyzerNode, CombineAnswerNode
+from graph.operations import  CombineAnswerNode
 
 __all__ = ["Node", "Edge", "Graph", "LLMNode", "CombineAnswerNode"]
