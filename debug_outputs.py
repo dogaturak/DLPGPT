500dd17f420c86e90730bcbb7bafd8fdbe1fc17b
dogaturak <d.turak@student.rug.nl>
2026-06-09 18:21:58 +0200

    , discarded

--- debug_outputs.py
+++ debug_outputs.py
@@ -14,13 +14,13 @@ async def main():
 
 
 
-    system_prompt=(
-            "You are a knowledgeable expert in question answering. I will ask you a question. "
-            "I will also give you 4 answers enumerated as A, B, C and D. Only one answer out of "
-            "the offered 4 is correct. You must choose the correct answer to the question. Your "
-            "response must be one of the 4 letters: A, B, C or D, corresponding to the correct "
-            "answer. Only one letter (A, B, C or D) is allowed in your answer."
-        ),
+    system_prompt = (
+        "You are a knowledgeable expert in question answering. I will ask you a question. "
+        "I will also give you 4 answers enumerated as A, B, C and D. Only one answer out of "
+        "the offered 4 is correct. You must choose the correct answer to the question. Your "
+        "response must be one of the 4 letters: A, B, C or D, corresponding to the correct "
+        "answer. Only one letter (A, B, C or D) is allowed in your answer."
+    )
 
     truthful = LLMNode(
        
